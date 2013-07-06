60bfefc97f3e57d58ff08a5f342393adfca4581e
oliveridario89 <oliveridario89@gmail.com>
2013-07-06 14:12:20 +0000

    First commit. started working on exceptions

---
+++ include/Infectorpp/InfectorHelpers.hpp
@@ -0,0 +1,96 @@
+/* Copyright (c) 2013 Dario Oliveri
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR */
+
+#pragma once
+#include <type_traits>
+#include <memory>
+#include "InfectorExceptions.hpp"
+
+namespace Infector{
+
+    template<typename... Others>
+    struct recursiveTest;
+
+    template<typename TEST>
+    struct recursiveTest<TEST> : std::integral_constant<bool, TEST::value> { };
+
+    template<typename TEST, typename... Others>
+    struct recursiveTest<TEST, Others...> : std::integral_constant<
+        bool, TEST::value && recursiveTest<Others...>::value> { };
+
+    /** Type test, there are few type traits that are checked, you can add
+    *   more tests if you need to do so. */
+    template <typename T, typename... Contracts>
+    bool type_tests(){
+
+        static_assert(  sizeof...(Contracts)>0 //if no contracts don't use "As"
+                      , " There must be at least 1 interface ");
+
+        static_assert(  recursiveTest<std::is_abstract<Contracts>...>()
+                      , " Contracts have to be abstract");
+
+        static_assert(  recursiveTest<std::is_base_of<Contracts,T>...>()
+                      , " Contracts must be base classes for T");
+
+        static_assert(  std::is_destructible<T>::value
+                      , " T must be destructible");
+
+        return true;
+    }
+
+    template <typename T>
+    bool reduced_type_tests(){
+
+        static_assert(  std::is_destructible<T>::value
+                      , " T must be destructible");
+
+        return true;
+    }
+
+    class IAnyShared{
+
+    public:
+        virtual void nothing()=0;
+        virtual ~IAnyShared() {
+            static_assert(  std::is_abstract<IAnyShared>::value
+                      , "IAnyShared must be abstract"); //strange.. a virtual method is required
+        }
+    };
+
+    /** Allows to have containers with different shared_ptr to different types. */
+    template <typename T>
+    struct AnyShared: public virtual IAnyShared{
+        virtual void nothing(){}
+        std::shared_ptr<T> ist;
+    };
+
+    class RecursionLimit{
+        static constexpr int max_depth = 20; //if you need to do so increase this limit to your will.
+        int current_depth = 0;
+    public:
+        void increment(){
+            current_depth++;
+            if(current_depth>max_depth)
+                throw Something();
+        }
+
+        void reset(){
+            current_depth = 0;
+        }
+    };
+
+} // namespace Infector
---
+++ include/Infectorpp/Infector_multi.hpp
@@ -0,0 +1,66 @@
+/* Copyright (c) 2013 Dario Oliveri
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR */
+
+#pragma once
+namespace Infector{
+
+    template <typename T, typename Contract >
+    void Container::bindAs(){
+        bool tests = type_tests<T,Contract>(); // Compile time tests.
+        (void) tests; //fix unused variable warning
+        must_not_have<Contract>(); // A contract can't be registered twice
+
+        typeMap[std::type_index(typeid(Contract))]
+                = Binding(std::type_index(typeid(T)), false);
+    }
+
+    template <typename T>
+    void Container::bindAsNothing(){
+        bool tests = reduced_type_tests<T>(); // Compile time tests.
+        (void) tests; //fix unused variable warning
+        must_not_have<T>(); // A Concrete type can't be registered twice.
+
+
+        typeMap[std::type_index(typeid(T))]
+                = Binding(std::type_index(typeid(T)), false);
+    }
+
+    template <typename T>
+    std::unique_ptr<T> Container::build_delegate(){
+        auto it = typeMap.find( std::type_index(typeid(T)) );
+        if( it==typeMap.end()) //"it" is concrete type, T's abstract (or concrete) one
+            launch_exception<Something>(); // TYPE NOT REGISTERED
+
+        if( it->second.single == true)
+            launch_exception<Something>(); // T is single but istantiated as multi
+        //at this point may happen that concrete type and abstract ones are the
+        //same.that's ok. (bindAsNothing)
+        auto it2 = callbacks.find( it->second.type ); //find constructor for concrete type
+
+        if( it2==callbacks.end() )
+            launch_exception<Something>(); // CONSTRUCTOR NOT WIRED
+
+        return std::unique_ptr<T>(reinterpret_cast<T*>( (it2->second)() )); // MAY THROW.. ok!
+    }
+
+    template <typename T>
+    std::unique_ptr<T> Container::build(){
+        limit.reset(); //need to cally only once, or in case of exception.
+        return build_delegate<T>();
+    }
+
+} // namespace Infector
