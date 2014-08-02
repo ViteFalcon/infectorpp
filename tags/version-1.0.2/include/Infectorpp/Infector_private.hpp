6cc1bfdfa535fb68a9b7576aae1cda4e115a89d2
oliveridario89@gmail.com <oliveridario89@gmail.com@79373fb4-4595-73dc-9982-fe1daf525664>
2014-08-02 21:46:51 +0000

    Tagged release 1.0.2. Include few bugfixes, small improvements and 1 important feature

---
+++ tags/version-1.0.2/include/Infectorpp/Infector_private.hpp
@@ -0,0 +1,124 @@
+/* Copyright (c) 2013 Dario Oliveri
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.*/
+
+#pragma once
+
+namespace Infector{
+
+    inline Container::~Container(){
+        auto it2 = singleIstances.end();
+        for( auto it=singleIstances.begin(); it!=it2; ++it){
+            if(it->second!=nullptr)
+                delete it->second;
+        }
+    }
+
+    template<typename Dummy>
+    bool Container::tryToGetSize( RecursionLimit *limit, int *size){
+        (void)limit; (void)size; //fix unused parameter warning.
+        return true;
+    }
+
+    template<typename Actual, typename Next, typename...Others>
+    bool Container::tryToGetSize( RecursionLimit *limit, int *size){
+        //get abstract type
+        auto it = typeMap.find( std::type_index(typeid(Actual)) );
+        if( it==typeMap.end())
+            return false; //abstract type not registered
+
+        auto it2 = recursionMap.find(  it->second.type ); //get concrete funct
+        if( it2==recursionMap.end())
+            return false; // concrete functor not created yet..
+
+        bool horizontalExpansion = tryToGetSize<Next,Others...>(limit,size);
+
+        (*size)+= sizeof(Actual);
+        (*limit).increment();
+        bool recursiveExpansion = (it2->second)(limit,size);
+        (*limit).decrement();
+
+        bool result = true&&horizontalExpansion&&recursiveExpansion;
+        setKnownSizeForType(it->second.type, result, (*size));
+        return result;
+    }
+
+    void Container::setKnownSizeForType( std::type_index t, bool known, int size){
+        if(known){
+            typeInfoMap[t] = size; //only if outer returned true => biggest minimum size!
+        }
+    }
+
+    void Container::processRecursionWeb(){
+        bool changes = true;
+        while( changes){
+            changes = false;
+            auto it = recursionWaitList.begin();
+            while (it!=recursionWaitList.end()){
+                auto it2=it;
+                it++;
+                RecursionLimit limit;
+                int            size = 0;
+                bool gotSize = (*it2)( &limit,&size);
+                if(gotSize) {
+                    changes = true;
+                    recursionWaitList.erase(it2);
+                }
+            }
+        }
+    }
+
+    template <typename T, typename... Dependencies>
+    void Container::wire(){
+        bool tests = reduced_type_tests<T>();
+        (void) tests; //fix unused variable warning
+
+        auto it2 = callbacks.find(std::type_index(typeid(T)));
+        if( it2!=callbacks.end())
+            launch_exception<ExWireAgain>();
+
+        auto it3 = recursionMap.find(std::type_index(typeid(T)));
+        if( it3==recursionMap.end()){
+            recursionMap[std::type_index(typeid(T))] =
+                    [this] (RecursionLimit *limit, int *size){
+                        (*size)+= sizeof(T);
+                        bool result =
+                            tryToGetSize<Dependencies...
+                                        ,DummyClass>(limit,size);
+
+                        return result;
+                    }; //now the function exist.
+            it3 = recursionMap.find(std::type_index(typeid(T)));
+            recursionWaitList.push_back(it3->second);
+        }
+
+        processRecursionWeb(); //do lots of checks during "wire"
+                               //find errors early!
+
+        // THROW... ok exception safe! Indipendently of parameters order
+        callbacks[std::type_index(typeid(T))] =
+                    [this] (EmplaceContext * context) {
+                        (void)context; //fix unused parameter warning.
+                        return reinterpret_cast<void*>(
+                            new T(UniqueOrShared<Dependencies>(this,context)...)
+                        );
+                    };
+    }
+
+} // namespace Infector
---
+++ tags/version-1.0.2/include/Infectorpp/Infector_single.hpp
@@ -0,0 +1,119 @@
+/* Copyright (c) 2013 Dario Oliveri
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE. */
+
+#pragma once
+
+namespace Infector{
+
+    template <typename T>
+    void Container::bindSingleAsNothing(){
+        bool tests = reduced_type_tests<T>();
+        (void) tests; //fix unused variable warning
+        must_not_have<T>();
+
+        typeMap[std::type_index(typeid(T))]
+                = Binding(std::type_index(typeid(T)), true);
+
+        try{
+            if(singleIstances.find( std::type_index(typeid(T)) )
+                                    ==singleIstances.end())
+                singleIstances[std::type_index(typeid(T))]
+                                = new AnyShared<T>();
+        }catch(std::exception & ex){
+            auto it2 = typeMap.find( std::type_index(typeid(T)) );
+            typeMap.erase(it2);
+            throw ex;
+        }
+    }
+
+    template <typename T, typename... Contracts>
+    void Container::bindSingleAs(){
+        bool tests = type_tests<T,Contracts...>(); // Compile time tests.
+        (void) tests;  //fix unused variable warning
+
+        bool success = false;
+        try{
+            success = resolve_multiple_inheritance<T,Contracts...>();
+        }catch(std::exception & ex){
+            rollback_multiple_inheritance<T,Contracts...>(); //revert changes
+            throw ex;
+        }
+        if(!success) //no changes.
+            launch_exception<ExExistingInterface>();
+
+        try{
+            if(singleIstances.find( std::type_index(typeid(T)) )
+                                    ==singleIstances.end())
+                singleIstances[std::type_index(typeid(T))]
+                               = new AnyShared<T,Contracts...>();
+        }catch(std::exception & ex){
+            rollback_multiple_inheritance<T,Contracts...>(); //revert changes
+            throw ex;
+        }
+    }
+
+    template <typename T>
+    std::shared_ptr<T> Container::buildSingle_delegate(){
+        auto it = typeMap.find( std::type_index(typeid(T)) );
+        if( it==typeMap.end()) //"it" is concrete type, T abstract one
+            launch_exception<ExBuildWhat>(); // TYPE NOT REGISTERED
+
+        if( it->second.single == false)
+            launch_exception<ExSingleMulti>(); // T must be single
+
+        //at this point may happen that concrete type and abstract ones are the
+        //same.that's ok. (bindSingleAsNothing)
+        auto it2 = callbacks.find(  it->second.type ); //find constructor
+
+        if( it2==callbacks.end())
+            launch_exception<ExNotWired>(); // CONSTRUCTOR NOT WIRED
+
+        auto it3 = singleIstances.find( it->second.type );
+
+        IAnyShared * any = it3->second;
+        T* myPointer = reinterpret_cast<T*>(
+                                    any->getPtr(std::type_index(typeid(T)))
+                                            );
+        if(myPointer==nullptr){
+            try{
+                any->setPtr( (it2->second)(nullptr) );
+                myPointer =  reinterpret_cast<T*>(
+                                    any->getPtr(std::type_index(typeid(T)))
+                                            );
+
+            }catch(std::exception & ex){
+                throw ex;
+            }
+        }
+
+        if(myPointer == nullptr)
+                launch_exception<ExAnySharedNullPtr>();
+
+        return std::shared_ptr<T> (
+                                any->getReferenceCounter(),
+                                myPointer );
+    }
+
+    template <typename T>
+    std::shared_ptr<T> Container::buildSingle(){
+        return buildSingle_delegate<T>();
+    }
+
+} // namespace Infector
